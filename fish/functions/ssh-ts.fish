eaff5bfae1d9966c2bfb5e9f86e0956f6d5e0cf7
Ybsin <ybsin@ybsin-lp.lan>
2025-01-18 06:45:40 +0100

    wip(ssh-ts): improving general structure and preview

--- fish/functions/ssh-ts.fish
+++ fish/functions/ssh-ts.fish
@@ -1,7 +1,25 @@
-function ssh-ts --description 'Finds all the peer devices in tailscale and connects to it'
-  # TODO : Add more use cases with args like sshing into other users if necessary or change the user.
-  # TODO : Make a better fzf window with tmux, maybe with ts status, test ping, add other functionalities with other fzf binds
 
-  set fzfcmd "enter:become(ssh {}),alt-enter:become(ssh root@{}),ctrl-e:change-preview( echo {\$json_ip} | jq '.Peer[] | select( .HostName == \"{}\")' | bat --style=numbers --color=always --language=json),ctrl-r:change-preview(Testing connection: {})+change-preview(ping {})"
-  set -Ux json_ip (tailscale status --json) && echo $json_ip | jq '.Peer[] | .HostName' --raw-output | fzf --multi --preview "echo {\$json_ip} | jq '.Peer[] | select( .HostName == \"{}\") | { Name: .HostName, OS:.OS, DNSName:.DNSName, IPs:.TailscaleIPs, Online: .Online}' | bat --style=numbers --color=always --language=json" --bind $fzfcmd ; set -e json_ip
+function ssh-ts --description 'Find and connect to peer devices in Tailscale'
+ 
+  set -Ux json_ip (tailscale status --json)
+
+  # Define a more user-friendly FZF bind string
+  set fzfcmd "
+    enter:execute(ssh {}),
+    alt-enter:execute(ssh root@{}),
+    ctrl-e:change-preview(echo \$json_ip | jq -r '.Peer[] | select(.TailscaleIPs[0] == \"{}\")' | bat --style=numbers --color=always --language=json),
+    ctrl-r:execute(ping {})+change-preview(echo 'Testing connection to {}' | bat --style=numbers --color=always --language=plaintext)
+  "
+
+  # Use FZF to display and select devices based on IP addresses
+  echo $json_ip | jq -r '.Peer[] | "\(.TailscaleIPs[0])\t\(.HostName)\t\(.Online)"' | fzf \
+    --header="Select a device (IP, Hostname, Online Status)" \
+    --delimiter="\t" \
+    --with-nth=2 \
+    --preview="echo \$json_ip | jq -r '.Peer[] | select(.TailscaleIPs[0] == \"{}\") | { Name: .HostName, OS: .OS, DNSName: .DNSName, IPs: .TailscaleIPs, Online: .Online }' | bat --style=numbers --color=always --language=json" \
+    --preview-window=up:3:wrap \
+    --bind=$fzfcmd
+
+  set -e json_ip
 end
+
