e4eb450d98a44eab07a482ce65b3ce2b34a80083
Ybsin <ybsin@ybsin-lp.lan>
2025-01-18 07:55:08 +0100

    improvement on config

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,15 @@
 if status is-interactive
   # Commands to run in interactive sessions can go here
   direnv hook fish | source
-  zoxide hook fish | source
+  zoxide init fish | source
   fzf --fish | source
-  gh completion -s fish
   
   if set -q $BW_SESSION
-    bw login | awk 'NR==4 {print $1 }'
+    set BW_SESSION (bw login | grep -m 1 -oP '(?<=BW_SESSION=")[^"]*' | awk 'NR==1 { print; exit }')
   end 
-
+  
   set -x BG_DIR $HOME/Pictures/Wallpapers/
+
 
   set DOCKER_CLI_EXPERIMENTAL enabled
   set XDG_CONFIG_HOME $HOME
@@ -27,16 +27,24 @@ if status is-interactive
 
   # Editor
   abbr l "lvim"
-  abbr c "bat"
-  abbr dd "lazydocker"
-  abbr dc "docker"
-  abbr dc "docker compose"
-  abbr dci "docker image"
-  abbr dcct "docker image"
-
-  abbr py "python"
-  abbr l "lvim"
   abbr c "cat"
+
+  # Docker
+  abbr dd "lazydocker"
+  abbr d "docker"
+  abbr dc "docker container"
+  abbr dim "docker image"
+  abbr din "docker inspect"
+  abbr dcc "docker compose"
+
+  # Podman
+  abbr p "podman"
+  abbr pmi "podman machine init"
+  abbr pms "podman machine start"
+  abbr pc "podman container" 
+  abbr pim "podman image"
+  abbr pin "podman inspect"
+  abbr pcc "podman compose"
 
   # Tf abbreviations
   abbr tf "terraform"
@@ -44,6 +52,16 @@ if status is-interactive
   abbr tfp "terraform plan"
   abbr tff "terraform fmt"
   abbr tfi "terraform init"
+
+  # Uv is a rust project to manage python packages
+  abbr py "python"
+  alias pip "uv pip"
+  alias venv "uv venv"
+
+  # ansible abbreviations
+  abbr a "ansible"
+  abbr ap "ansible-playbook"
+  abbr ag "ansible-galaxy"
 
   if test -z $TMUX_SOCKET
     tmux
