0b1dc73bcc296945929cf118db612f7b29b14b53
Ybsin <ybsin@ybsin-lp.lan>
2025-01-18 07:39:13 +0100

    fix(): changed tmux active identification

--- fish/config.fish
+++ fish/config.fish
@@ -4,17 +4,12 @@ if status is-interactive
   zoxide hook fish | source
   fzf --fish | source
   gh completion -s fish
-  if set -q $TMUX_SOCKET
-    tmux
-  end
-
+  
   if set -q $BW_SESSION
     bw login | awk 'NR==4 {print $1 }'
   end 
 
-  fish_add_path $HOME/.local/share/nvm/v23.3.0/bin/
   set -x BG_DIR $HOME/Pictures/Wallpapers/
-
 
   set DOCKER_CLI_EXPERIMENTAL enabled
   set XDG_CONFIG_HOME $HOME
@@ -49,6 +44,10 @@ if status is-interactive
   abbr tfp "terraform plan"
   abbr tff "terraform fmt"
   abbr tfi "terraform init"
+
+  if test -z $TMUX_SOCKET
+    tmux
+  end
 
 end
 
