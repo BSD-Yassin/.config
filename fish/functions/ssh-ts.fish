77a6fef051348cd1711844205a0ef5bcecbc505f
Ybsin <ybsin@ybsin-lp.lan>
2025-01-18 06:27:07 +0100

    added ssh-ts function

---
+++ fish/functions/ssh-ts.fish
@@ -0,0 +1,7 @@
+function ssh-ts --description 'Finds all the peer devices in tailscale and connects to it'
+  # TODO : Add more use cases with args like sshing into other users if necessary or change the user.
+  # TODO : Make a better fzf window with tmux, maybe with ts status, test ping, add other functionalities with other fzf binds
+
+  set fzfcmd "enter:become(ssh {}),alt-enter:become(ssh root@{}),ctrl-e:change-preview( echo {\$json_ip} | jq '.Peer[] | select( .HostName == \"{}\")' | bat --style=numbers --color=always --language=json),ctrl-r:change-preview(Testing connection: {})+change-preview(ping {})"
+  set -Ux json_ip (tailscale status --json) && echo $json_ip | jq '.Peer[] | .HostName' --raw-output | fzf --multi --preview "echo {\$json_ip} | jq '.Peer[] | select( .HostName == \"{}\") | { Name: .HostName, OS:.OS, DNSName:.DNSName, IPs:.TailscaleIPs, Online: .Online}' | bat --style=numbers --color=always --language=json" --bind $fzfcmd ; set -e json_ip
+end
